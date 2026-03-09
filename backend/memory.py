20ef605a840cace102b80fefe02061b226e419d3
sahil5568 <sahillodhi784@gmail.com>
2026-03-09 17:44:11 +0530

    fix redis connection for render

--- backend/memory.py
+++ backend/memory.py
@@ -3,12 +3,12 @@ import json
 import os
 
 # connect to redis
-redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST", "localhost"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
+redis_url = os.getenv("REDIS_URL")
+
+redis_client = redis.from_url(
+    redis_url,
     decode_responses=True
 )
-
 
 def get_history(session_id):
     data = redis_client.get(session_id)
@@ -17,7 +17,6 @@ def get_history(session_id):
         return json.loads(data)
 
     return []
-
 
 def save_history(session_id, history):
     redis_client.set(session_id, json.dumps(history))
